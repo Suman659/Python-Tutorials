1fdb3fe2fdf3e73b1f071816e3411699c8b19f2f
Suman Mahato <103480485+Suman659@users.noreply.github.com>
2024-01-24 00:32:03 +0530

    Add files via upload

---
+++ Comments, Escape Sequences & Print Statement.py
@@ -0,0 +1,46 @@
+#Comments, Escape Sequences & Print Statement
+
+# Comments;
+
+#      { # } is used for commenting a single line
+#      { """ } is used for multi line comment
+""""
+Multi
+Line
+Comment
+
+"""
+
+# Escape Sequences;
+
+#1. \n (New Line)
+print("Hello\nWorld")
+
+#2. \t (New Tab)
+print("Hello\tWorld")
+
+#3. \000 (Octal Value)
+print("\110\145\154\154\157")
+
+#4. \xhh (Hex Value)
+print("\x48\x65\x6c\x6c\x6f")
+
+#5. \\ (Backslash)
+print("Hello\\")
+
+#6. \' (Single Quote)
+print("Suman\'s Phone")
+
+
+# Print Statement;
+
+#1 Printing a single statement
+print("Hello World")
+
+#2 Printing 2 or more statements
+print("Hello World")
+print("Good Morning")
+
+#3 If Statement needs to be within same line
+print("Hello World", end=" ")
+print("Good Morning")
