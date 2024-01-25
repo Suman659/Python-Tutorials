d903b39645e19d8ec040e4c8a96f91fffef94f67
Suman Mahato <103480485+Suman659@users.noreply.github.com>
2024-01-25 19:25:14 +0530

    Add files via upload

---
+++ Variables, Datatypes and Typecasting.py
@@ -0,0 +1,22 @@
+#Variable
+
+a="Hello "
+b=10
+c=5.2
+print(type(a))
+print(a)
+print(a+str(b))
+print(b+c)
+print(b+int(c))
+print(a+str(b)+str(c))
+
+
+#DataTypes
+
+'''
+string- str()
+float- float()
+integer- int()
+'''
+
+Typecasting
