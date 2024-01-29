6a9be5680a5c90d6d8e2ff8247b40d85f2357a87
Suman Mahato <103480485+Suman659@users.noreply.github.com>
2024-01-29 23:53:09 +0530

    Add files via upload

---
+++ Python Lists And List Functions.py
@@ -0,0 +1,6 @@
+# Lists
+
+grocery=['wheat','rice','potato','tomato','paneer',100]     # Used to store data at a single place we can keep strings and integers both
+print(grocery)
+print(grocery[1])       # Using index we can also print the specific string from the list needed
+
